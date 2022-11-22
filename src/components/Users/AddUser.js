9a76a51715262a6202ab9ceddae7af6d31a9a980
rakshitar1135 <rakshitaraju2000@gmail.com>
2022-11-22 18:22:15 +0530

    ADDING A WRAPPER

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -4,6 +4,7 @@ import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
+import Wrapper from '../Helpers/Wrappers';
 const AddUser = (props) => {
  const[enteredUsername,setEnteredUsername]=useState('');
  const[enteredAge,setEnteredAge]=useState('');
@@ -42,7 +43,7 @@ const AddUser = (props) => {
   }
 
   return (
-    <div>
+    <Wrapper>
     {error && <ErrorModal title={error.title} message={error.message} onConfrim={errorHandler}/>}
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
@@ -53,7 +54,7 @@ const AddUser = (props) => {
         <Button type="submit">Add User</Button>
       </form>
     </Card>
-    </div>
+    </Wrapper>
   );
 };
 
